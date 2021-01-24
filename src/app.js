0418b8ef3bcd5d5edb8503444b3bdb5e8814fd16
Laura Tannahill <lntannahill@gmail.com>
2021-01-24 16:05:51 -0400

    try again

--- src/app.js
+++ src/app.js
@@ -22,16 +22,26 @@ app.use(
 );
 
 // PG database client/connection setup
-const { Pool } = require("pg");
-const dbParams = require("../lib/db.js");
-const db = new Pool({
-  host: dbParams.host,
-  port: dbParams.port,
-  user: dbParams.user,
-  password: dbParams.password,
-  database: dbParams.database,
-});
-db.connect();
+// const { Pool } = require("pg");
+// const dbParams = require("../lib/db.js");
+// const db = new Pool({
+//   host: dbParams.host,
+//   port: dbParams.port,
+//   user: dbParams.user,
+//   password: dbParams.password,
+//   database: dbParams.database,
+// });
+// db.connect();
+
+//HEROKU -- comment for local deploy
+const pg = require("pg");
+pg.defaults.ssl = true;
+const connectionString = 'postgres://lrlddljhohuvcl:5d021e95c405ac97486a7037e7ecd23e938f814d40a8385b3d60daa1ecd36826@ec2-52-54-174-5.compute-1.amazonaws.com:5432/de6cts8lnm086b';
+
+const db = new pg.Pool({
+  connectionString: connectionString
+})
+db.connect().then(() => console.log('db connected')).catch(error => console.log(error));
 
 app.use(logger("dev"));
 app.use(express.json());
